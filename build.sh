87b4b0af10ba8c42865821f3f22c15b1ef35c086
Chris Berthiaume <chrisbee@uw.edu>
2021-12-01 18:00:08 -0800

    Support unannotated tags in build product filename

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build tsdata command-line tool for 64-bit MacOS and Linux
 
-VERSION=$(git describe --long --dirty)
+VERSION=$(git describe --tags --long --dirty)
 GOOS=darwin GOARCH=amd64 go build -o "tsdata.${VERSION}.darwin-amd64/tsdata" cmd/tsdata/main.go || exit 1
 GOOS=linux GOARCH=amd64 go build -o "tsdata.${VERSION}.linux-amd64/tsdata" cmd/tsdata/main.go || exit 1
 zip -q -r "tsdata.${VERSION}.darwin-amd64.zip" "tsdata.${VERSION}.darwin-amd64" || exit 1
